eda221ae977445c1fbc609ce54e278a1cb860da7
dennis <ndungudennis250@gmail.com>
2025-02-26 16:51:22 +0300

    remove navbar

--- wardrobe-frontend/src/App.vue
+++ wardrobe-frontend/src/App.vue
@@ -3,12 +3,23 @@ import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 import Navbar from "@/views/Layout/Navbar.vue";
 import Footer from "@/views/Layout/Footer.vue";
+import {useAuthStore} from "@/store/auth.ts";
+import {onMounted, ref} from "vue";
+import type {User} from "@/types";
+
+const auth = useAuthStore();
+
+const user = ref<User| null>(null);
+
+
 </script>
 
 <template>
   <header></header>
-      <Navbar/>
+      <Navbar v-if="auth.isLoggedIn"/>
+    <main>
       <RouterView/>
-      <Footer/>
+    </main>
+      <Footer v-if="auth.isLoggedIn"/>
 </template>
 
