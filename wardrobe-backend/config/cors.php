cd9b511a536467923f3d3eb136544c28ebd114fc
dennis <ndungudennis250@gmail.com>
2025-02-28 13:52:52 +0300

    hh

--- wardrobe-backend/config/cors.php
+++ wardrobe-backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://wardrobe-management-system-sigma.vercel.app', 'http://localhost:3000'],
 
     'allowed_origins_patterns' => [],
 
