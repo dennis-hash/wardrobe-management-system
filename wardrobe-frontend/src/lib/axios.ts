0ba7b1a4f3fde7508e98d966afe7549531a4832e
dennis <ndungudennis250@gmail.com>
2025-02-28 19:58:48 +0300

    fix csrf error

--- wardrobe-frontend/src/lib/axios.ts
+++ wardrobe-frontend/src/lib/axios.ts
@@ -3,8 +3,8 @@ import {useAuthStore} from "@/store/auth.ts";
 import router from "@/router";
 
 const axiosInstance = axios.create({
-  //baseURL: "https://wardrobe-management-system.onrender.com/api",
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://wardrobe-management-system.onrender.com/api",
+  //baseURL: "http://localhost:8000/api",
   withCredentials: true,
   withXSRFToken: true,
   xsrfCookieName: "XSRF-TOKEN",
@@ -13,10 +13,16 @@ const axiosInstance = axios.create({
   headers: {
 
     Accept: "application/json",
-
+    'X-XSRF-TOKEN':get_cookie('XSRF-TOKEN'),
 
   },
 });
+
+function get_cookie(name: string) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop()?.split(';').shift();
+}
 
 
 
